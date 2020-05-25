9a84f538478b6b5d84d1bd4d1a9b2b5423f68467
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-05-25 08:15:31 +0530

    Create _dbconnect.php

---
+++ _dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+// Script to connect to the database
+$servername =  "localhost";
+$username = "root";
+$password = "";
+$database = "idiscuss";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+
+
+
+
+
+
+
+
+?>
