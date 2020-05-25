5761ab3586b7a92c24ac89f4ff5b7e250a56bd0f
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-05-25 08:16:13 +0530

    Create _header.php

---
+++ partials/_header.php
@@ -0,0 +1,53 @@
+<?php
+
+echo '
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<a class="navbar-brand" href="/Forum">iDiscuss</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="/Forum">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Categories
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link " href="contact.php" >Contact</a>
+      </li>
+        
+    </ul>
+    <div class="row mx-2">
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <button class="btn btn-outline-success ml-2" data-toggle="modal" data-target="#loginModal">Login</button>
+    <button class="btn btn-outline-success mx-2"  data-toggle="modal" data-target="#signupModal">Signup</button>
+
+    </div>
+   
+</div>
+</nav>
+
+
+';
+include 'partials/_loginModal.php';
+include 'partials/_signupModal.php';
+
+
+?>
