c2cded386e1d18e6afc9990f20cbcad25858c5e9
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-06-11 17:11:27 +0530

    Update _loginModal.php

--- partials/_loginModal.php
+++ partials/_loginModal.php
@@ -9,22 +9,19 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form>
+            <form action="/forum/partials/_handleLogin.php" method="post">
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="loginEmail">Email address</label>
+                        <input type="email" class="form-control" id="loginEmail" name="loginEmail" aria-describedby="emailHelp">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label for="loginPass">Password</label>
+                        <input type="password" class="form-control" id="loginPass" name="loginPass">
                     </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
+               
 
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
