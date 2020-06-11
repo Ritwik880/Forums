7c6549878aefea03e92b87f02c19eb37edf6c279
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-06-11 17:07:41 +0530

    Create _handleSignup.php

---
+++ partials/_handleSignup.php
@@ -0,0 +1,38 @@
+<?php
+$showError = "false";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    include '_dbconnect.php';
+    $user_email = $_POST['signupEmail'];
+    $pass = $_POST['signupPassword'];
+    $cpass = $_POST['signupcPassword'];
+
+    // Check whether this email exists
+    $existSql = "select * from `users` where user_email = '$user_email'";
+    $result = mysqli_query($conn, $existSql);
+    $numRows = mysqli_num_rows($result);
+    if($numRows>0)
+{
+    $showError = "Email already in use";
+}
+else{
+    if($pass == $cpass){
+        $hash = password_hash($pass, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO `users` ( `user_email`, `user_pass`, `timestamp`)
+         VALUES ( '$user_email', '$hash', current_timestamp())";
+         $result = mysqli_query($conn, $sql);
+         if($result){
+             $showAlert = true;
+             header("Location: /Forum/index.php?signupsuccess=true");
+             exit();
+         }
+    }
+    else{
+        $showError = "Password do not match";
+
+
+    }
+}
+header("Location: /forum/index.php?signupsuccess=false&error=$showError");
+
+}
+?>
