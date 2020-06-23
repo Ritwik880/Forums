869f661790575ef6b25ef4565a0f990cf62d4345
Ritwik Sinha <rittiksinha12@gmail.com>
2020-06-23 12:06:47 +0530

    Update threads.php

--- threads.php
+++ threads.php
@@ -9,26 +9,51 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
             integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <style>
+        #ques {
+            min-height: 433px;
+        }
+        </style>
 
         <title>Forums</title>
 </head>
 
 <body>
+    <?php include 'partials/_dbconnect.php'; ?>
     <?php include 'partials/_header.php'; ?>
-    <?php include '_dbconnect.php'; ?>
 
     <?php
     $id = $_GET['catid'];
-      $sql = "SELECT * FROM `categories` WHERE category_id= $id";
-      $result = mysqli_query($conn, $sql);
-      while($row = mysqli_fetch_assoc($result)){
-          $catname = $row['category_name'];
-          $catdesc = $row['category_description'];
+    $sql = "SELECT * FROM `categories` WHERE category_id= $id";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $catname = $row['category_name'];
+        $catdesc = $row['category_description'];
+    }
+    ?>
 
-      }
+    <?php
+    $showAlert = false;
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method == 'POST') {
+        $th_title = $_POST['title'];
+        $th_desc = $_POST['desc'];
+        $sql = "INSERT INTO `threads` ( `thread_title`, `thread_desc`, `thread_cat_id`,
+         `thread_user_id`, `timestamp`) VALUES ( '$th_title', '$th_desc', '$id', '0', current_timestamp())";
+        $result = mysqli_query($conn, $sql);
+        $showAlert = true;
+        if ($showAlert) {
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success!</strong> Your thread has been added! Please wait for community to respond.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>';
+        }
+    }
     ?>
     <!-- Category container starts here -->
-    <div class="container my-3">
+    <div class="container my-4">
         <div class="jumbotron">
             <h1 class="display-4">Welcome to <?php echo $catname; ?> forums</h1>
             <p class="lead"><?php echo $catdesc; ?></p>
@@ -43,45 +68,96 @@
             <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a>
         </div>
     </div>
+    <?php
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        echo '<div class="container">
+        <h1 class="py-2">Start a Discussion</h1>
 
-    <div class="container">
-        <h1 class="py-2">Browse Questions</h1>
-        <?php
-        $id = $_GET['catid'];
-      $sql = "SELECT * FROM `threads` WHERE thread_cat_id=$id";
-      $result = mysqli_query($conn, $sql);
-      while($row = mysqli_fetch_assoc($result)){
-          $id = $row['thread_id'];
-          $title = $row['thread_title'];
-          $desc = $row['thread_desc'];
-
-
-
-        echo '<div class="media my-3">
-            <img src="img/userdefault.webp" width="54px" class="mr-3" alt="...">
-            <div class="media-body">
-                <h5 class="mt-0"><a class="text-dark" href="thread.php">'. $title .'</a></h5>
-                '.$desc.'
-            </div>
-        </div>';
+        <form action="' . $_SERVER["REQUEST_URI"] . '" method="post">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Problem Title</label>
+        <input type="text" class="form-control" id="title" name="title" aria-describedby="title">
+        <small id="emailHelp" class="form-text text-muted">Keep your title as short and crisp as
+            possible</small>
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlTextarea1">Elaborate your Concern</label>
+        <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>';
+    } else {
+        echo '
+        <div class="container">
+        <h1 class="py-2">Start a Discussion</h1>
+        <p class="lead">You are not logged in. Please login to be able to start a discussion</p>
+    </div>
+        
+        ';
     }
 
     ?>
-        
-    
 
-    <?php include 'partials/_footer.php'; ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-    </script>
+
+    </div>
+    <div class="container mb-5" id="ques">
+        <h1 class="py-2">Browse Questions</h1>
+        <?php
+        $id = $_GET['catid'];
+        $sql = "SELECT * FROM `threads` WHERE thread_cat_id=$id";
+        $result = mysqli_query($conn, $sql);
+        $noResult = true;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $noResult = false;
+            $id = $row['thread_id'];
+            $title = $row['thread_title'];
+            $desc = $row['thread_desc'];
+            $thread_time = $row['timestamp'];
+            $thread_user_id = $row['thread_user_id'];
+            $sql2 = "SELECT user_email FROM `users` WHERE sno='$thread_user_id'";
+            $result2 = mysqli_query($conn, $sql2);
+            $row2 = mysqli_fetch_assoc($result2);
+
+
+
+
+            echo '<div class="media my-3">
+            <img src="img/userdefault.webp" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+            <p class="font-weight-bold my-0"> Asked by: '. $row2['user_email'] . ' at ' . $thread_time . '</p>
+                <h5 class="mt-0"><a class="text-dark" href="thread.php?threadid=' . $id . '">' . $title . '</a></h5>
+                ' . $desc . '
+                </div>
+
+        </div>';
+        }
+        // echo var_dump($noResult);
+        if ($noResult) {
+            echo '<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <p class="display-4">No Threads Found</p>
+          <p class="lead">Be the first one to ask a question</p>
+        </div>
+      </div>';
+        }
+
+        ?>
+
+
+
+
+        <?php include 'partials/_footer.php'; ?>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+        </script>
 </body>
 
 </html
