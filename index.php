662f3bab08a4d8cb0c85a999bfd134010e644c24
Ritwik Sinha <rittiksinha12@gmail.com>
2020-06-23 12:07:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,17 @@
 
 <head>
     <! -- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <title>Forums</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <title>Forums</title>
 </head>
 
 <body>
+    <?php include 'partials/_dbconnect.php'; ?>
     <?php include 'partials/_header.php'; ?>
-    <?php include '_dbconnect.php'; ?>
 
 
     <!-- slider starts here -->
@@ -49,34 +47,34 @@
         </a>
     </div>
     <!-- Category container starts here -->
-    <div class="container my-3">
+    <div class="container my-3" id="ques">
         <h2 class="text-center my-3">iDiscuss - Categories</h2>
         <div class="row my-3">
-                    <!-- Use a for loop to iterate through categories -->
-                    <!-- Fetch All the Categories -->
-            <?php 
+            <!-- Use a for loop to iterate through categories -->
+            <!-- Fetch All the Categories -->
+            <?php
             $sql = "SELECT * FROM `categories`";
             $result = mysqli_query($conn, $sql);
-            while($row = mysqli_fetch_assoc($result)){
-                // echo $row['category_id'];
-                // echo $row['category_name'];
+
+            while ($row = mysqli_fetch_assoc($result)) {
+
                 $id = $row['category_id'];
                 $cat = $row['category_name'];
                 $desc = $row['category_description'];
 
-                echo 
-                '<div class="col-md-4">
+                echo
+                    '<div class="col-md-4">
                     <div class="card" style="width: 18rem;">
                         <img src="https://source.unsplash.com/500x400/?' . $cat . ',coding" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"><a href = "threads.php?catid= ' . $id . '">' . $cat . '</a></h5>
-                            <p class="card-text">' . substr($desc,0, 90) . '...</p>
+                            <p class="card-text">' . substr($desc, 0, 90) . '...</p>
                             <a href="threads.php?catid= ' . $id . '" class="btn btn-primary">View Threads</a>
                         </div>
                     </div>
                 </div>';
-
             }
+
             ?>
         </div>
     </div>
@@ -84,14 +82,11 @@
     <?php include 'partials/_footer.php'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
 </body>
 
